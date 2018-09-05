9b6a79238078ff81a6d7ff383d4d1b7585f70ec4
Dex Hunter <dixingxu@gmail.com>
2018-09-05 15:46:02 +0800

    update abi in test

--- mainchain/config.js
+++ mainchain/config.js
@@ -1,5 +1,5 @@
 const config = {
-  chain: 'http://115.28.153.39:1337',
+  chain: 'http://stellarcoder.com:1337',
   privateKey: '0x5f0258a4778057a8a7d97809bd209055b2fbafa654ce7d31ec7191066b9225e6',
   contractAddress: ''
 }
